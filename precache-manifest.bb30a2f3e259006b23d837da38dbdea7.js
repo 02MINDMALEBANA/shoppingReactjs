198b6cdc5ff29978feaac49ef04fa9109700ba56
02MINDMALEBANA <malebanakgopo@gmail.com>
2022-06-15 12:46:56 +0200

    Updates

---
+++ precache-manifest.bb30a2f3e259006b23d837da38dbdea7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "a46a4db3a13c02aea6a5",
+    "url": "/shoppingReactjs/static/css/main.ef9e09a8.chunk.css"
+  },
+  {
+    "revision": "a46a4db3a13c02aea6a5",
+    "url": "/shoppingReactjs/static/js/main.ae28c986.chunk.js"
+  },
+  {
+    "revision": "6dfefe118ae0a1a03434",
+    "url": "/shoppingReactjs/static/js/runtime~main.a26b337a.js"
+  },
+  {
+    "revision": "3d14556411700f39d7cf",
+    "url": "/shoppingReactjs/static/js/2.2c98887f.chunk.js"
+  },
+  {
+    "revision": "f610709ef7b15d7cd37f",
+    "url": "/shoppingReactjs/static/js/3.c3bc911a.chunk.js"
+  },
+  {
+    "revision": "21926079968c5aba2547de741e1234b2",
+    "url": "/shoppingReactjs/static/media/wiredKeyboard.21926079.jfif"
+  },
+  {
+    "revision": "7a4e0ee2117c166966d905e597937796",
+    "url": "/shoppingReactjs/static/media/wirelesskeyboard.7a4e0ee2.jfif"
+  },
+  {
+    "revision": "67408c3746801b477bf8ca4442b54b67",
+    "url": "/shoppingReactjs/static/media/wirelessMouse.67408c37.jfif"
+  },
+  {
+    "revision": "48cbfb530b724d59ca84d28c5c5cfc5d",
+    "url": "/shoppingReactjs/static/media/wiredMOuse.48cbfb53.jfif"
+  },
+  {
+    "revision": "b238b8f774e7d39dcf93b7543f4e7801",
+    "url": "/shoppingReactjs/static/media/case.b238b8f7.jfif"
+  },
+  {
+    "revision": "35e77226145e3c627d400fe80e034716",
+    "url": "/shoppingReactjs/static/media/computercase.35e77226.jfif"
+  },
+  {
+    "revision": "faec8ee564c0bc3d82fc0702929acedd",
+    "url": "/shoppingReactjs/static/media/monitor.faec8ee5.jfif"
+  },
+  {
+    "revision": "f3486e040811bc3d06bc0af185bf3e4d",
+    "url": "/shoppingReactjs/static/media/samsungMonitor.f3486e04.jfif"
+  },
+  {
+    "revision": "4fbd55e3e19cbf72809d7a4a672e29dc",
+    "url": "/shoppingReactjs/static/media/corei5.4fbd55e3.jfif"
+  },
+  {
+    "revision": "e87683d72e40504a4332ab9ff28d37e2",
+    "url": "/shoppingReactjs/static/media/i7.e87683d7.jfif"
+  },
+  {
+    "revision": "d1bfef01b67930d2bff3d34b0be5c010",
+    "url": "/shoppingReactjs/static/media/external hard drive.d1bfef01.jfif"
+  },
+  {
+    "revision": "c564951b1cfd383dd27fee30fee0fd67",
+    "url": "/shoppingReactjs/static/media/hardDrive.c564951b.jfif"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/shoppingReactjs/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "03f46279f672b958ca64c3f43c37100f",
+    "url": "/shoppingReactjs/static/media/back.03f46279.jpg"
+  },
+  {
+    "revision": "0ca95b40abb219e74d04e7d625194eba",
+    "url": "/shoppingReactjs/index.html"
+  }
+];
